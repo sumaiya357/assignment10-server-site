8007a0ff78ac91e08874a8a3e04b104279203065
sumaiya246 <rahmansumaiya75@gmail.com>
2022-10-27 22:49:23 +0600

    fake data added

--- index.js
+++ index.js
@@ -2,12 +2,18 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT||5000;
 
+const data =require('./data.json')
+const cors = require('cors');
 
 app.get('/',(req,res)=>{
     res.send('API is running')
 })
 
+app.use(cors())
 
+app.get('/data',(req,res)=> {
+    res.send(data)
+})
 app.listen(port, ()=>{
     console.log('Course loading on port')
 })
