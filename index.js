cb232a800b92f9de4678b35f9ddcf015ad8b5c22
sumaiya246 <rahmansumaiya75@gmail.com>
2022-10-28 19:53:39 +0600

    gitignore added

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const port = process.env.PORT||5000;
 
 const data =require('./data.json')
+const category = require('./category.json')
 const cors = require('cors');
 
 app.get('/',(req,res)=>{
@@ -13,6 +14,12 @@ app.use(cors())
 
 app.get('/data',(req,res)=> {
     res.send(data)
+})
+
+app.get('/category/:id',(req,res)=>{
+    const id = req.params.id;
+    const selectedCategory = category.filter( single_category => single_category.id ===id)
+    res.send( selectedCategory)
 })
 app.listen(port, ()=>{
     console.log('Course loading on port')
